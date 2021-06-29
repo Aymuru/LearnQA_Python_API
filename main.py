272e3b35054521c3c579b8631d219b45ba398868
Anna Zavyazkina <amuriabaiko@gmail.com>
2021-06-29 23:18:35 +0300

    refresh

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello from Anna Zavyazkina1236789uuuuuu!")
+from json.decoder import JSONDecodeError
+import requests
+
+response = requests.get("https://playground.learnqa.ru/api/get_text")
+print(response.text)
+
+try:
+    parsed_response_text = response.json()
+    print(parsed_response_text)
+except JSONDecodeError:
+    print("Is not a JSON format, bitch.")
+
