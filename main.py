9a791abfdcc71a6155103c6f9634946dd0b04ac8
Anna Zavyazkina <amuriabaiko@gmail.com>
2021-06-29 23:26:09 +0300

    refresh

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-<<<<<<< HEAD
 #print("Hello from Anna Zavyazkina!")
 
-=======
 from json.decoder import JSONDecodeError
->>>>>>> develop
+
 import requests
 
 response = requests.get("https://playground.learnqa.ru/api/get_text")
 print(response.text)
-<<<<<<< HEAD
-=======
 
 try:
     parsed_response_text = response.json()
@@ -17,4 +13,4 @@ try:
 except JSONDecodeError:
     print("Is not a JSON format, bitch.")
 
->>>>>>> develop
+
