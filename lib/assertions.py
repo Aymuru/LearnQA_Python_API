22bda35d2e0c378076cf83b7ac9800df5e7b0312
Anna Zavyazkina <amuriabaiko@gmail.com>
2021-07-16 11:11:01 +0300

    add libs

---
+++ lib/assertions.py
@@ -0,0 +1,66 @@
+from requests import Response
+import json
+
+
+class Assertions:
+    @staticmethod
+    def assert_json_value_by_name(response: Response, name, expected_value, error_message):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not a JSON format. Response text is '{response.text}'"
+
+        assert name in response_as_dict, f"Response doesn't have key '{name}'"
+        assert response_as_dict[name] == expected_value, error_message
+
+    @staticmethod
+    def assert_json_has_key(response: Response, name):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not a JSON format. Response text is '{response.text}'"
+
+        assert name in response_as_dict, f"Response doesn't have key '{name}'"
+
+    @staticmethod
+    def assert_json_has_keys(response: Response, names: list):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not a JSON format. Response text is '{response.text}'"
+
+        for name in names:
+            assert name in response_as_dict, f"Response doesn't have key '{name}'"
+
+    @staticmethod
+    def assert_json_has_no_key(response: Response, name):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not a JSON format. Response text is '{response.text}'"
+
+        assert name not in response_as_dict, f"Response shouldn't have key '{name}'. But it's present"
+
+    @staticmethod
+    def assert_json_has_no_keys(response: Response, names: list):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not a JSON format. Response text is '{response.text}'"
+
+        for name in names:
+            assert name not in response_as_dict, f"Response shouldn't have key '{name}'. But it's present"
+
+    @staticmethod
+    def assert_code_status(response: Response, expected_status_code):
+        assert response.status_code == expected_status_code, \
+            f"Unexpected status code! Expected: {expected_status_code}. Actual: {response.status_code}"
+
+    @staticmethod
+    def assert_no_add_symbol_in_email(response: Response, symbol):
+        try:
+            exist = response.json()['email']
+        except json.JSONDecodeError:
+            assert False, f"Response is not a JSON format. Response text is '{response.text}'"
+
+        assert symbol in exist, f"Email doesn't have symbol '{symbol}'"
